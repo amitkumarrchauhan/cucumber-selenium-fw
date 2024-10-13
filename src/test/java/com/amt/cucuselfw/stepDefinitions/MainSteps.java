c2e94b311a45e5abd7e2955d0caa83a09a45d1e7
Amitkumar Chauhan <chauhan.amitkumar@theoptimum.net>
2024-10-14 07:36:24 +0800

    Change done to reuse steps buy introducing simple regex for placeholder

--- src/test/java/com/amt/cucuselfw/stepDefinitions/MainSteps.java
+++ src/test/java/com/amt/cucuselfw/stepDefinitions/MainSteps.java
@@ -9,29 +9,37 @@ public class MainSteps {
   @Given("User is on Netbanking landing page")
   public void user_is_on_netbanking_landing_page() {
     System.out.println("user is on netbanking landing page");
-//     Write code here that turns the phrase above into concrete actions
-//    throw new io.cucumber.java.PendingException();
+    //     Write code here that turns the phrase above into concrete actions
+    //    throw new io.cucumber.java.PendingException();
   }
 
+  /*
   @When("User login into application")
   public void user_login_into_application() {
     System.out.println("user login into application");
 //    Write code here that turns the phrase above into concrete actions
 //    throw new io.cucumber.java.PendingException();
   }
+*/
+  @When("User login into application with {string} and password {string}")
+  public void user_login_into_application_with_and_password(String username, String password) {
+    System.out.printf("User login into application with %s and password %s", username, password);
+    // Write code here that turns the phrase above into concrete actions
+//    throw new io.cucumber.java.PendingException();
+  }
 
   @Then("Home Page is displayed")
   public void home_page_is_displayed() {
     System.out.println("home page is displayed");
-//     Write code here that turns the phrase above into concrete actions
-//    throw new io.cucumber.java.PendingException();
+    //     Write code here that turns the phrase above into concrete actions
+    //    throw new io.cucumber.java.PendingException();
   }
 
   @Then("Cards are displayed")
   public void cards_are_displayed() {
     System.out.println("cards are displayed");
-//     Write code here that turns the phrase above into concrete actions
-//    throw new io.cucumber.java.PendingException();
+    //     Write code here that turns the phrase above into concrete actions
+    //    throw new io.cucumber.java.PendingException();
   }
 
 }

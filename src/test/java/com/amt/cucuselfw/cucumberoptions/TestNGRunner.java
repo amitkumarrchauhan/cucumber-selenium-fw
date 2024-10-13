bf6e2e12b3f47f03937de537e5b3baae878b3487
Amitkumar Chauhan <chauhan.amitkumar@theoptimum.net>
2024-10-14 07:17:55 +0800

    Changes done to add blank MainSteps

--- src/test/java/com/amt/cucuselfw/cucumberoptions/TestNGRunner.java
+++ src/test/java/com/amt/cucuselfw/cucumberoptions/TestNGRunner.java
@@ -1,7 +1,9 @@
 package com.amt.cucuselfw.cucumberoptions;
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/java/features")
-public class TestNGRunner {
+@CucumberOptions(features = "src/test/java/com/amt/cucuselfw/features",
+    glue = "com.amt.cucuselfw.stepDefinitions", monochrome = true)
+public class TestNGRunner extends AbstractTestNGCucumberTests {
 }
---
+++ src/test/java/com/amt/cucuselfw/stepDefinitions/MainSteps.java
@@ -0,0 +1,37 @@
+package com.amt.cucuselfw.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MainSteps {
+
+  @Given("User is on Netbanking landing page")
+  public void user_is_on_netbanking_landing_page() {
+    System.out.println("user is on netbanking landing page");
+//     Write code here that turns the phrase above into concrete actions
+//    throw new io.cucumber.java.PendingException();
+  }
+
+  @When("User login into application")
+  public void user_login_into_application() {
+    System.out.println("user login into application");
+//    Write code here that turns the phrase above into concrete actions
+//    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("Home Page is displayed")
+  public void home_page_is_displayed() {
+    System.out.println("home page is displayed");
+//     Write code here that turns the phrase above into concrete actions
+//    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("Cards are displayed")
+  public void cards_are_displayed() {
+    System.out.println("cards are displayed");
+//     Write code here that turns the phrase above into concrete actions
+//    throw new io.cucumber.java.PendingException();
+  }
+
+}
